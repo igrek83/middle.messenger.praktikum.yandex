6bcfe662af2077b768d8f18c9b3b7309edba4588
Igor Golanov <53408396+igrek83@users.noreply.github.com>
2022-10-03 00:07:19 +0300

    Revert "Sprint 1"

--- src/components/nav-link/nav-link.tmp.js
+++
@@ -1,6 +0,0 @@
-import handlebars from 'handlebars';
-import './nav-link.scss';
-
-export const navLinkTmp = `<a href={{ to }} id={{id}} class="nav-link">{{ content }}</a>`
-
-handlebars.registerPartial('nav-link', navLinkTmp);
